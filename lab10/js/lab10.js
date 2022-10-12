90bddcf0ced4070a513cd635a99695214bac084a
malieebanculla <mebanculla@csumb.edu>
2022-10-11 19:26:05 -0700

    changed js

--- lab10/js/lab10.js
+++ lab10/js/lab10.js
@@ -1,33 +1,63 @@
+document.addEventListener('DOMContentLoaded', () => {
+
 //1. one of the course boxes 
 //2. mouseover
 //3. the color of the box changes
+let button = document.querySelector(".itembox");
+
+let highlightBox = (e) => {
+    button.style.border = "thick solid grey";
+};
+let off = (e) => {
+    button.style.border = "thin solid grey";
+};
+
+button.addEventListener("mouseover", highlightBox);
 
 
 //1. one of the course boxes
 //2. mouseout
 //3. the box goes back to the way it was before 
 
+button.addEventListener('mouseout', off);
+
 //1. the course box 
 //2. click
 //3. info panel shows up 
 ///update course name, description, and picture
+
+
 let showPanel = (e) => {
     console.log(e);
 
-let panel = document.querySelector("#infopanel");
-    console.log(panel)
+    let panel = document.querySelector("#infopanel");
+    console.log(panel);
     panel.style.display = "block";
+    
 
-let courseName = document.querySelector('#courseName');
+    let courseName = document.querySelector('#courseName');
     console.log(courseName);
     courseName.textContent = "CST 252";
 
-let courseDescription = document.querySelector('#courseDescription');
+    let courseDescription = document.querySelector('#courseDescription');
     console.log(courseDescription);
-    courseDescription.textContent = "Javascript Class"
+    courseDescription.textContent = "Javascript Class";
 }
+
+button.addEventListener('click', showPanel); 
+
+
 
 //1. the exit button 
 //2. click 
 //3. info panel disappears 
 
+let exitButton = document.querySelector('#exitbutton');
+    exitButton.addEventListener('click', closePanel);
+
+let closePanel = (e) => {
+    infoPanel.style.display = "none";  
+}
+
+
+})
